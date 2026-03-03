cdb20acd3423dac49810743ebc0dc66ccd2678e0
ahmadzaidz <23734664+ahmadzaidz@users.noreply.github.com>
2026-03-03 12:52:27 +0800

    text node class, test cases for equality

---
+++ src/main.py
@@ -0,0 +1,6 @@
+from textnode import TextType, TextNode
+
+def main():
+    print (TextNode("This is some anchor text", TextType("link"), "https://www.boot.dev"))
+
+main()
---
+++ src/test_textnode.py
@@ -0,0 +1,35 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_not_eq_type(self):
+        node = TextNode("this is a text node", TextType.BOLD)
+        node2 = TextNode("this is a text node", TextType.LINK)
+        self.assertNotEqual(node, node2)
+    
+    def test_not_eq_text(self):
+        node = TextNode("this is a text node", TextType.PLAIN)
+        node2 = TextNode("this is a different text node", TextType.PLAIN)
+        self.assertNotEqual(node, node2)
+    
+    def test_one_url(self):
+        node = TextNode("this is a text node", TextType.LINK, "www.google.com")
+        node2 = TextNode("this is a text node", TextType.LINK)
+        self.assertNotEqual(node, node2)
+    
+    def test_eq_url(self):
+        node = TextNode("this is a text node", TextType.LINK, "www.google.com")
+        node2 = TextNode("this is a text node", TextType.LINK, "www.google.com")
+        self.assertEqual(node, node2)
+    
+
+
+if __name__ == "__main__":
+    unittest.main()
