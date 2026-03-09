0ecb911eedaadf78af146796a887feb674064c61
ahmadzaidz <23734664+ahmadzaidz@users.noreply.github.com>
2026-03-09 15:11:44 +0800

    attempt 4: fixed missing argument

--- src/main.py
+++ src/main.py
@@ -2,10 +2,9 @@ from website_functions import  prepare_and_copy, generate_pages_recursive
 import sys
 
 def main():
-    try:
-        basepath = sys.argv[1]
-    except:
-        basepath = "/"
+    
+    basepath = sys.argv[1]
+    
     prepare_and_copy("static", "docs")
     generate_pages_recursive("content", "template.html", "docs", basepath)
     
--- src/website_functions.py
+++ src/website_functions.py
@@ -9,13 +9,13 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, bas
         from_path = os.path.join(dir_path_content, content)
         to_path = os.path.join(dest_dir_path, content)
         if os.path.isfile(from_path) and from_path[-3:] == ".md":
-            generate_page(from_path, template_path, f"{to_path[:-3]}.html")
+            generate_page(from_path, template_path, f"{to_path[:-3]}.html", basepath)
         elif os.path.isdir(from_path):
             os.makedirs(to_path, exist_ok=True)
             generate_pages_recursive(from_path, template_path, to_path, basepath)
 
 
-def generate_page(from_path, template_path, dest_path, basepath="/"):
+def generate_page(from_path, template_path, dest_path, basepath):
     print (f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path, "r") as f:
         src = f.read()
